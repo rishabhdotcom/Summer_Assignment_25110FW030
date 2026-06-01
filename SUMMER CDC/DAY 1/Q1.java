097de3f89092e2a327ffac03402d16f7cbfa99fd
rishabhdotcom <pg.rishabh17@gmail.com>
2026-06-02 00:13:52 +0530

    Add files via upload

--- SUMMER CDC/DAY 1/Q1.java
+++ SUMMER CDC/DAY 1/Q1.java
@@ -4,12 +4,12 @@ public class Q1 {
 public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter N: ");
+        System.out.print("Enter the N: ");
         int n = sc.nextInt();
 
         int sum = n * (n + 1) / 2;
 
-        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+        System.out.println("Sum of the first " + n + " natural numbers = " + sum);
 
         sc.close();
     }
