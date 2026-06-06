b30c3558612da18fe419d231219431d3d1708b3e
rishabhdotcom <pg.rishabh17@gmail.com>
2026-06-06 13:01:24 +0530

    Update Q15.java

--- SUMMER CDC/DAY4/Q15.java
+++ SUMMER CDC/DAY4/Q15.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Q15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
-
         int originalNum = num;
         int digits = 0;
         int sum = 0;
-
-        // Count digits
         int temp = num;
         while (temp != 0) {
             digits++;
@@ -23,7 +19,6 @@ public class Q15 {
             sum += Math.pow(digit, digits);
             temp /= 10;
         }
-
         if (sum == originalNum) {
             System.out.print(originalNum);
             System.out.print(" is an Armstrong Number");
@@ -31,7 +26,6 @@ public class Q15 {
             System.out.print(originalNum);
             System.out.println(" is not an Armstrong Number");
         }
-
         sc.close();
     }
 }
